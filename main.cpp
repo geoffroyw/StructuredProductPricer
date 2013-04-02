9b2ce9296e0e474b9139489e2310a7735f1da858
Geoffroy Weyer <webweyer@gmail.com>
2013-04-02 14:58:16 +0200

    Bonus Cliquer pricer

--- main.cpp
+++ main.cpp
@@ -7,12 +7,15 @@
 using namespace std;
 
 void BestPlusPrice();
+void BonusCliquetPrice();
 
 int main( int argc, const char* argv[] )
 {
 	printf( "\nHello World\n\n" );
 	
 	BestPlusPrice();
+	cout << "\n\n\n";
+	BonusCliquetPrice();
 
 	system("PAUSE");
 }
@@ -26,8 +29,8 @@ void BestPlusPrice() {
 	int nbTimeStep = 7;
 	int nbSimulation = 1000;
 	double sum_payoffs(0);
-	double riskFreeRate = 0.2;
-	double capital = 100;
+	double riskFreeRate = 0.03;
+	double capital = 100.0;
 
 
 	vector<double> spot_prices(nb_ASJ);
@@ -182,8 +185,9 @@ void BestPlusPrice() {
 				payoff += capital*0.07*exp(-riskFreeRate*i);
 			}
 		}
-		payoff+=capital*(1+best_pf_performance+0.07)*exp(-riskFreeRate*(nbTimeStep-1));
+		payoff+=capital*(1+best_pf_performance)*exp(-riskFreeRate*(nbTimeStep));
 		payoffs.push_back(payoff);
+
 		sum_payoffs+=payoff;
 	}
 
@@ -203,4 +207,95 @@ void BestPlusPrice() {
 	cout << "Intervalle de confiance à 95%\n";
 	cout << "Borne inf : " << prix-2*var/sqrt(nbSimulation) << "\n";
 	cout << "Borne sup : " << prix+2*var/sqrt(nbSimulation) << "\n";
+}
+
+
+void BonusCliquetPrice() {
+	double maturity = 7.0;
+	double performance_obj = 0.35;
+	int nbTimeStep = 52*maturity;
+	int nbSimulation = 1000;
+	double sum_payoffs(0);
+	double riskFreeRate = 0.03;
+	double spotPrice = 100.0;
+	double low_barrier = 80;
+	double high_barrier = 120;
+	double vol = 0.3;
+	vector<double> payoffs;
+	double payoff;
+
+
+	bool low_barrier_reach = false;
+	bool high_barrier_reach = true;
+
+	boost::mt19937 rng; 
+	boost::normal_distribution<> nd(0.0, 1.0);
+	boost::variate_generator<boost::mt19937&, boost::normal_distribution<> > var_nor(rng, nd);
+
+
+	double delta_t = maturity/nbTimeStep;
+	double r;
+	double sd;
+	double S_t;
+
+	for(int k=0;k<nbSimulation;k++) {
+		high_barrier_reach = false;
+		low_barrier_reach = false;
+
+
+		//On simule les trajectoires
+		r = (riskFreeRate-0.5*pow(vol,2))*delta_t;
+		sd = vol*sqrt(delta_t);
+		S_t = spotPrice;
+		for(int j = 1; j<nbTimeStep;j++) {
+			S_t= S_t*exp(r+sd*var_nor());
+
+			if(S_t>=high_barrier) {
+				high_barrier_reach = true;
+			}
+			if(S_t<=low_barrier) {
+				low_barrier_reach = true;
+			}
+		}
+
+		payoff = 0;
+		if(!low_barrier_reach) {
+			if(S_t>high_barrier) {
+				payoff=S_t;
+			}
+			else {
+				payoff=high_barrier;
+			}
+		}
+		else {
+			if(high_barrier_reach) {
+				payoff = high_barrier;
+			}
+			else {
+				payoff=S_t;
+			}
+		}
+
+		payoff*=exp(-riskFreeRate*maturity);
+		sum_payoffs+=payoff;
+		payoffs.push_back(payoff);
+	}
+
+	double prix = sum_payoffs/nbSimulation; 
+	cout << "Prix : ";
+	cout << prix;
+	cout << "€\n";
+
+	//Variance des payoffs
+	double var = 0;
+	for(int i=0;i<nbSimulation;i++){
+		var+=pow((payoffs[i]-sum_payoffs/nbSimulation),2);
+	}
+	var/=(nbSimulation-1);
+	var=sqrt(var);
+
+	cout << "Intervalle de confiance à 95%\n";
+	cout << "Borne inf : " << prix-2*var/sqrt(nbSimulation) << "\n";
+	cout << "Borne sup : " << prix+2*var/sqrt(nbSimulation) << "\n";
+
 }
