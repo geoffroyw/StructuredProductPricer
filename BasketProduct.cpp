e9aaea20ee83ed9fad7063667215ae9e56e5f746
Geoffroy Weyer <webweyer@gmail.com>
2013-04-20 14:57:41 +0200

    Fixes Cholesky

--- BasketProduct.cpp
+++ BasketProduct.cpp
@@ -6,6 +6,7 @@
 #include <boost/random/normal_distribution.hpp>
 #include <stdexcept>
 #include <ctime>
+#include <boost/format.hpp>
 
 using namespace std;
 
@@ -26,19 +27,32 @@ void BasketProduct::cholesky() {
 			cholM(i,j)=0;
 		}
 	}
-	for(int i=0;i<cholM.size1();i++) {
-		for(int j=i;j<cholM.size2();j++) {
-			x = correlations(i,j);
-			for(int k=0;k<i-1;k++) {
-				x = x-cholM(i,k)*cholM(j,k);
-			}
-			if(i==j) {
-				cholM(i,i)=sqrt(x);
-			}
-			else {
-				cholM(j,i)=x/cholM(i,i);
-			}
+	
+	for(int j=0;j<cholM.size1();j++) {
+		x = 0.0;
+		for(int k = 0; k<j-1; k++) {
+			x += cholM(j,k)*cholM(j,k);
+			cout << j <<  "\n" << k << "\n";
+			cout << cholM(j,k) << "\n";
 		}
+		cholM(j,j) = correlations(j,j)-x;
+		if(cholM(j,j)<=0) {
+			cout << "pb" << "\n";
+			cout << j << "\n";
+			cout << boost::format("%1$.4f") % x << "\n";
+			cout << boost::format("%1$.4f") % correlations(j,j) << "\n";
+		}
+		cholM(j,j) = sqrt(cholM(j,j));
+
+
+		for(int i=j+1; i<cholM.size1(); i++) {
+			x = 0.0;
+			for(int k =0; k<j-1;k++) {
+				x += cholM(i,k)*cholM(j,k);
+			}
+			cholM(i,j) = (correlations(i,j)-x)/cholM(j,j);
+		}
+
 	}
 }
 
@@ -54,8 +68,8 @@ void BasketProduct::simulateRandVars() {
 	for(int k = 0;k<nbSimulation;k++) {
 		for(int l =0; l<nbTimestep; l++) {
 			for(int i = 0; i<nbAsj; i++) {
-				//randVars.push_back(var_nor());
-				randVars.push_back(varNorm());
+				randVars.push_back(var_nor());
+				//randVars.push_back(varNorm());
 				temp = 0.0;
 				for(int j=0;j<=i;j++) {
 					temp+=randVars[j]*cholM(i,j);
